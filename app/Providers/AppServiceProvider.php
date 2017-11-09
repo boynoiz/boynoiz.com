883ef5d3340a4656a4dd8703e5b50af334e2ed52
bOynOiz <boynoiz@gmail.com>
2017-11-09 19:29:54 +0700

    Added dev tool

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,11 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Barryvdh\Debugbar\ServiceProvider as DebugbarServiceProvider;
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
+use Clockwork\Support\Laravel\ClockworkServiceProvider;
+use Laravel\Dusk\DuskServiceProvider;
+use Laravel\Tinker\TinkerServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,6 +32,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->environment('dev', 'testing')) {
+            $this->app->register(IdeHelperServiceProvider::class);
+            $this->app->register(TinkerServiceProvider::class);
+            $this->app->register(DebugbarServiceProvider::class);
+            $this->app->register(DuskServiceProvider::class);
+            $this->app->register(ClockworkServiceProvider::class);
+        }
     }
 }
