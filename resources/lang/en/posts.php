340e7b549252cb8f688c2f441a6e55faa4cb1559
bOynOiz <boynoiz@gmail.com>
2017-11-18 17:00:35 +0700

    Add posts search lang

--- resources/lang/en/posts.php
+++ resources/lang/en/posts.php
@@ -1,30 +1,32 @@
 <?php
 
 return [
-  'publish' => 'Publish',
-  'delete' => 'Delete',
-  'delete_thumbnail' => 'Delete this image',
-  'edit' => 'Edit',
-  'last_posts' => 'Last post',
-  'create' => 'New post',
-  'created' => 'Post created successfully',
-  'updated' => 'Post updated successfully',
-  'deleted' => 'Post deleted successfully',
-  'empty' => 'There is no article for the moment.',
-  'new_posts' => 'new article|new articles',
-  'count' => ':count article|:count articles',
-  'show' => 'See post',
+    'publish' => 'Publish',
+    'delete' => 'Delete',
+    'delete_thumbnail' => 'Delete this image',
+    'edit' => 'Edit',
+    'last_posts' => 'Last post',
+    'create' => 'New post',
+    'created' => 'Post created successfully',
+    'updated' => 'Post updated successfully',
+    'deleted' => 'Post deleted successfully',
+    'empty' => 'There is no article for the moment.',
+    'new_posts' => 'new article|new articles',
+    'count' => ':count article|:count articles',
+    'show' => 'See post',
+    'search' => 'Search ...',
+    'search_results' => ':count post found|:count posts found',
 
-  'attributes' => [
-    'title' => 'Title',
-    'content' => 'Content',
-    'author' => 'Author',
-    'thumbnail' => 'Post image',
-    'posted_at' => 'Post at',
-  ],
+    'attributes' => [
+        'title' => 'Title',
+        'content' => 'Content',
+        'author' => 'Author',
+        'thumbnail' => 'Post image',
+        'posted_at' => 'Post at',
+    ],
 
-  'placeholder' => [
-    'title' => 'Your title',
-    'content' => 'Your content'
-  ]
+    'placeholder' => [
+        'title' => 'Your title',
+        'content' => 'Your content'
+    ]
 ];
